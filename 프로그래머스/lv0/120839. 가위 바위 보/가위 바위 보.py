bc8fb4851445b3a28de7dde194bfb5b2a73871f4
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-01-05 08:55:43 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,10 @@
+def solution(rsp):
+    answer = ""
+    for i in range(len(rsp)):
+        if rsp[i] == "2":
+            answer += "0"
+        elif rsp[i] == "5":
+            answer += "2"
+        else:
+            answer += "5"
+    return answer
