f5ecd29a55fb01dafac8bd72a09ccd4880fa4e22
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-01-05 07:17:36 +0900

    [level 0] Title: 제곱수 판별하기, Time: 1.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,8 +1,3 @@
 def solution(n):
-    answer = []
-    for i in range(1,10001):
-        answer.append(i*i)
-    if n in answer:
-        return 1
-    else:
-        return 2
+    answer = 0
+    return answer
