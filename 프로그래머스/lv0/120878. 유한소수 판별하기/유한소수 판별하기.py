6e646da458c266faf6b64b36f312cd92fdea9f0e
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-01-15 21:26:17 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,11 @@
+from math import gcd
+def solution(a, b):
+    b //= gcd(a, b)
+    while b % 2 == 0:
+        b //= 2
+    while b % 5 == 0:
+        b //= 5
+    if b == 1:
+        return 1
+    else:
+        return 2
