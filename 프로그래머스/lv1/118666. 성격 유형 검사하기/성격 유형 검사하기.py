602dbe9a21a60c3287ccc0c48f08ab0e3c9a8e30
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-02-02 02:33:54 +0900

    Update 성격 유형 검사하기.py
    코드 정리 및 주석 추가

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,25 +1,32 @@
 def solution(survey, choices):
-    # for i in range(len(survey)):
-    # survey[i][0]: 비동의
-    # survey[i][1]: 동의
-
-    # choices[i]: 1: 매우 비동의
-    # choices[i]: 2: 비동의
-    # choices[i]: 3: 약간 비동의
-    # choices[i]: 4: 모르겠음
-    # choices[i]: 5: 약간 동의
-    # choices[i]: 6: 동의
-    # choices[i]: 7: 매우 동의
-    # ...
     """
+    1 : RT TR
+    2 : CF FC
+    3 : JM MJ
+    4 : AN NA
+
+    3점: 매우 동의 매우 비동의
+    2점: 동의 비동의
+    1점: 약간 동의 약간 비동의
+    0점: 모르겠음
+
+    choices[i]: 1: 매우 비동의
+    choices[i]: 2: 비동의
+    choices[i]: 3: 약간 비동의
+    choices[i]: 4: 모르겠음
+    choices[i]: 5: 약간 동의
+    choices[i]: 6: 동의
+    choices[i]: 7: 매우 동의
+    ...
+
     [5,3,2,7,5]
     =>
     ["약간 동의", "약간 비동의", "비동의", "매우 동의", "약간 동의"]
     ["AN", "CF", "MJ", "RT", "NA"]
     "AN"
     => 5이므로 약간 동의.
-    =>비동의일경우 어피치 1점
-    =>동의일경우 네오 1점
+    => 비동의일경우 어피치 1점
+    => 동의일경우 네오 1점
     => 네오 1점
 
     "CF"
@@ -30,28 +37,25 @@ def solution(survey, choices):
 
     "MJ"
     => 2이므로 비동의
-    =>비동의일경우 무지 2점
-    =>동의일경우 제이지 2점
+    => 비동의일경우 무지 2점
+    => 동의일경우 제이지 2점
     => 무지 2점
 
     "RT"
     => 7이므로 매우 동의
-    =>비동의일경우 라이언 3점
-    =>동의일경우 튜브 3점
+    => 비동의일경우 라이언 3점
+    => 동의일경우 튜브 3점
     => 튜브 3점
 
     "NA"
     => 5이므로 약간 동의
-    =>비동의일경우 네오 1점
-    =>동의일경우 어피치 1점
+    => 비동의일경우 네오 1점
+    => 동의일경우 어피치 1점
     => 어피치 1점
 
     문자열 [i][0]항은 비동의
     문자열 [i][1]항은 동의
-
-
     """
-
     answer = ""
     scoreList = [["R", 0], ["T", 0], ["C", 0], ["F", 0],
                  ["J", 0], ["M", 0], ["A", 0], ["N", 0]]
@@ -59,59 +63,38 @@ def solution(survey, choices):
         for j in range(8):
             if choices[i] == 1:  # 매우 비동의
                 if survey[i][0] == scoreList[j][0]:
-                    for k in range(8):
-                        if survey[i][0] == scoreList[k][0]:
-                            scoreList[k][1] += 3
+                    scoreList[j][1] += 3
             elif choices[i] == 2:  # 비동의 # MJ
                 if survey[i][0] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][0]:
-                            scoreList[k][1] += 2
+                    scoreList[j][1] += 2
             elif choices[i] == 3:  # 약간 비동의
                 if survey[i][0] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][0]:
-                            scoreList[k][1] += 1
+                    scoreList[j][1] += 1
             elif choices[i] == 4:  # 모르겠음
                 if survey[i][0] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][0]:
-                            scoreList[k][1] += 0
+                    scoreList[j][1] += 0
             elif choices[i] == 5:  # 약간 동의
+                # 약간 동의 "AN"
+                # => 5이므로 약간 동의.
+                # => 비동의일경우 어피치 1점
+                # => 동의일경우 네오 1점
+                # => 네오 1점
                 if survey[i][1] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][1]:
-                            scoreList[k][1] += 1
-
+                    scoreList[j][1] += 1
             elif choices[i] == 6:  # 동의
                 if survey[i][1] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][1]:
-                            scoreList[k][1] += 2
+                    scoreList[j][1] += 2
             elif choices[i] == 7:  # 매우 동의
                 if survey[i][1] == scoreList[j][0]:
-                    for k in range(8):
-                        if scoreList[k][0] == survey[i][1]:
-                            scoreList[k][1] += 3
-    for i in range(0,8,2):
-        if scoreList[i][1] > scoreList[i+1][1]:
+                    scoreList[j][1] += 3
+    for i in range(0, 8, 2):
+        if scoreList[i][1] > scoreList[i + 1][1]:
             answer += scoreList[i][0]
-        elif scoreList[i][1] < scoreList[i+1][1]:
-            answer += scoreList[i+1][0]
-        elif scoreList[i][1] == scoreList[i+1][1]:
-            if ord(scoreList[i][0]) > ord(scoreList[i+1][0]):
+        elif scoreList[i][1] < scoreList[i + 1][1]:
+            answer += scoreList[i + 1][0]
+        elif scoreList[i][1] == scoreList[i + 1][1]:
+            if ord(scoreList[i][0]) > ord(scoreList[i + 1][0]):
                 answer += scoreList[1][0]
-            elif ord(scoreList[i][0]) < ord(scoreList[i+1][0]):
+            elif ord(scoreList[i][0]) < ord(scoreList[i + 1][0]):
                 answer += scoreList[i][0]
-
-    # if scoreList[0][1] > scoreList[1][1]:
-    #     answer += scoreList[0][1]
-    # elif scoreList[0][1] < scoreList[1][1]:
-    #     answer += scoreList[1][1]
-    # elif scoreList[0][1] == scoreList[1][1]:
-    #     if ord(scoreList[0][1]) > ord(scoreList[1][1]):
-    #         answer += scoreList[1][1]
-    #     elif ord(scoreList[0][1]) < ord(scoreList[1][1]):
-    #         answer += scoreList[0][1]
-    print("scoreList:", scoreList)
     return answer
