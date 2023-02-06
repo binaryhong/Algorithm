20256444ee3f8c013f7a872b6af0f88ace6d8b78
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-02-07 04:27:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,14 +1,4 @@
+import re
+
 def solution(s):
-    listS = list(s)
-    for i in range(len(listS)):
-        if listS[i].isupper():
-            listS[i] = listS[i].lower()
-    for i in range(len(listS)):
-        try:
-            if listS[i - 1] == " ":
-                listS[i] = listS[i].upper()
-        except:
-            pass
-    if listS[0].islower():
-        listS[0] = listS[0].upper()
-    return "".join(listS)
+    return re.sub(r'\b(\d*[a-zA-Z]+)\b', lambda x: x.group(1).capitalize(), s)
