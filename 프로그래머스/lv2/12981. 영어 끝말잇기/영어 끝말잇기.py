304ab0e36585e0f33478b8d2e60d3d54aaa81d6c
binaryhong <102649855+binaryhong@users.noreply.github.com>
2023-02-13 02:03:31 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,11 @@
 def solution(n, words):
-    answer = [0,0]
     stack = [words[0]]
     for i in range(1, len(words)):
+        # stack의 마지막 원소의 마지막 문자가 현재 words의 첫번째 문자와 같고,
+        # word가 stack 없을경우
         if stack[-1][-1] == words[i][0] and words[i] not in stack:
             stack.append(words[i])
+            # print("stack:", stack)
         else:
-            answer[0] = (i % n) + 1
-            answer[1] = i // n + 1
-            return answer
-    return answer
+            return [i % n + 1, i // n + 1]  # 번호, 차례 
+    return [0, 0]
