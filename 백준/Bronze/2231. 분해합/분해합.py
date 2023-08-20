772e80e0f1613646fec658b7fbfaafb9e3e7ac3e
Hong <102649855+binaryhong@users.noreply.github.com>
2023-08-20 19:51:18 +0900

    [Bronze II] Title: 분해합, Time: 1268 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(1, N+1):   # 해당 분해합의 생성자 찾기
+    num = sum((map(int, str(i))))  # i의 각 자릿수를 더함
+    num_sum = i + num  # 분해합 = 생성자 + 각 자릿수의 합
+    # i가 작은 수부터 차례로 들어가므로 처음으로 분해합과 입력값이 같을때가 가장 작은 생성자를 가짐
+    if num_sum == N:
+        print(i)
+        break
+    if i == N:  # 생성자 i와 입력값이 같다는 것은 생성자가 없다는 뜻
+        print(0)
