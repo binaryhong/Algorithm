7f897e2ac142695366f5a256ba9bdbb97786058a
Hong <102649855+binaryhong@users.noreply.github.com>
2023-09-14 15:33:20 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,12 @@
+number = input()
+ans = [0] * 10
+for i in range(len(number)):
+    num = int(number[i])
+    if num == 6 or num == 9:
+        if ans[6] <= ans[9]:
+            ans[6] += 1
+        else:
+            ans[9] += 1
+    else:
+        ans[num] += 1
+print(max(ans))
