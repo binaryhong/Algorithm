8ae73eafc0d8ec3915e231bd5dc603f30d2090c7
Hong <102649855+binaryhong@users.noreply.github.com>
2025-05-18 15:09:00 +0900

    [level 2] Title: 가장 큰 수, Time: 63.30 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,3 @@
 def solution(numbers):
-    newNumberList = [str(num) for num in numbers]
-    # 핵심: 문자열을 3번 반복해서 비교 (999, 343434, 303030 ...)
-    newNumberList.sort(key=lambda x: x*3, reverse=True)
-    answer = ''.join(newNumberList)
-    # 000...0 같은 케이스 처리
-    return '0' if answer[0] == '0' else answer
+    answer = ''
+    return answer
